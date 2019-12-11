37773c5011ceee667f93cfbc3cf3455d3373a3a0
moritzebeling <git@moritzebeling.com>
2019-12-11 12:55:13 +0100

    add years to participants template

--- site/snippets/t_participants.php
+++ site/snippets/t_participants.php
@@ -13,7 +13,7 @@ Recieving:
 <article id="participants" class="participants page span2 add-nav" data-count="<?= $count ?>">
 
 	<header>
-		<h3><?= $page->title() ?></h3>
+		<h3><?= $page->title() ?> 1976–2013</h3>
 	</header>
 
 	<!-- <section class="filter">
